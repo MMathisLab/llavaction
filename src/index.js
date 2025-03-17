eba9f32c700ff8b27272f53c322a45854ce19502
MMathisLab <mathis@rowland.harvard.edu>
2025-03-17 17:15:11 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
 
-// No need to import static CSS files here since they are loaded in index.html
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
