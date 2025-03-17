ee1006d7378e84787dcec82bb6956c9a2e3a3f81
MMathisLab <mathis@rowland.harvard.edu>
2025-03-17 17:38:35 +0100

    clean

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './App.css';
 import App from './App';
 
 const container = document.getElementById('root');
+if (!container) {
+  throw new Error('Failed to find the root element');
+}
+
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
